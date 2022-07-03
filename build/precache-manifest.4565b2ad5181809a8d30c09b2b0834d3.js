4172c655484e0fee225b1b9e377dcc7aa54e3f4f
Mimameid <ibrahimkeser@outlook.de>
2022-07-03 16:14:33 +0200

    Adding build to repository

---
+++ build/precache-manifest.4565b2ad5181809a8d30c09b2b0834d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bacf31c6c64b27cd9abd",
+    "url": "/static/js/main.b3d15678.chunk.js"
+  },
+  {
+    "revision": "bfc167aa708f39956e41",
+    "url": "/static/js/runtime~main.13a91d0b.js"
+  },
+  {
+    "revision": "ba50fe7db8122263ec82",
+    "url": "/static/css/2.4bc2c2a4.chunk.css"
+  },
+  {
+    "revision": "ba50fe7db8122263ec82",
+    "url": "/static/js/2.ea6c4f84.chunk.js"
+  },
+  {
+    "revision": "d67932c3736a16fd5cdd",
+    "url": "/static/js/3.05e4965e.chunk.js"
+  },
+  {
+    "revision": "67bc013e269aab4260033ec92a735e85",
+    "url": "/index.html"
+  }
+];
